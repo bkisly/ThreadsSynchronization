c81f720534e88637490fb64f60c290fa925158e8
bkisly <b.kisly@gmail.com>
2022-12-14 10:42:39 +0100

    Initialized repository

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void producer(int minAmount, int maxAmount);
+void consumer(int minAmount, int maxAmount);
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+void producer(int minAmount, int maxAmount) {
+
+}
+
+void consumer(int minAmount, int maxAmount) {
+
+}
